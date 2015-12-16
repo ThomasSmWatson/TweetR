fef914f1661b89c8a084e8d1e5f9e336518a7eb7
Thomas Watson <thomas.sm.watson@gmail.com>
2015-12-16 22:18:53 +0000

    Connected to mongoDb via mongoose, added connection data to config.json

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 var config = require('./config/server_config.json');
+mongoose.connect('mongodb://'+config.db.address+':'+config.db.port);
+var db = mongoose.connection;
+db.on('error',console.error.bind(console,'Connection Error: '));
+db.once('open',function(callback){
+	console.log('Connected to MongoDb at: '+config.db.address+', Port:'+config.db.port);
+});
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
